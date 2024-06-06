6c28e1ac1a2219148c11f5d3e3283b134c3c7ca8
taemochi12 <himanshi4821@gmail.com>
2024-06-07 02:05:19 +0530

    Added insertion and deletion

--- LinkedList/slltemplate.cpp
+++ LinkedList/slltemplate.cpp
@@ -24,6 +24,17 @@ void display(ListNode* head)
     cout<<"null"<<endl;
 }
 
+int length(ListNode* head)
+{
+    ListNode *temp=head;
+    int l=0;
+    while(temp!=nullptr)
+    {
+        l++;
+    }
+    return l;
+}
+
 ListNode* reverse(ListNode *head)
 {
     ListNode* current=head,*temp=head,*prev=nullptr;
@@ -35,6 +46,77 @@ ListNode* reverse(ListNode *head)
         current=temp;
     }
     return prev;
+}
+
+ListNode* insert(ListNode* head,ListNode *node,int k)
+{
+    if(head==nullptr) head=node;
+    else{
+        if(k==0) 
+        {
+            node->next=head;
+            head=node;
+        }
+        else if(k==length(head)-1)
+        {
+            ListNode* temp=head;
+            while(temp->next!=nullptr)
+            {
+                temp=temp->next;
+            }
+            temp->next=node;
+            node->next=nullptr;
+        }
+        else{
+            ListNode* temp=head;
+            int p=0;
+            while(p<k-1)
+            {
+                temp=temp->next;
+            }
+            node->next=temp->next;
+            temp->next=node;
+        }
+    }
+    return head;
+}
+
+ListNode* del(ListNode* head,int k)
+{
+    if(head==nullptr) return head; 
+    else if(head->next==nullptr) head=nullptr;
+    else{
+        if(k==0)
+        {
+            ListNode* temp=head;
+            head=head->next;
+            free(temp);
+        }
+        else if(k==length(head)-1)
+        {
+            ListNode* temp=head;
+            while(temp->next->next!=nullptr)
+            {
+                temp=temp->next;
+            }
+            ListNode* delt=temp->next;
+            temp->next=nullptr;
+            free(delt);
+        }
+        else
+        {
+          ListNode* temp=head;
+            int p=0;
+            while(p<k-1)
+            {
+                temp=temp->next;
+            }  
+            ListNode* delt=temp->next;
+            temp->next=temp->next->next;
+            free(delt);
+        }
+    }
+    return head;
 }
 
 int main() {
